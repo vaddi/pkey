8054e2dd08921268d99c2ab80db064490bc8d0e7
vaddi <vaddi@mvattersen.de>
2014-05-12 22:29:01 +0200

    cleanup config.php

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ define ("SITE_NAME", "product-keymanager");		// Der Name der Seite
 define ("SITE_TITLE", "Produkt-Schl&uuml;sselverwaltung"); // Untertitel oder Slogan
 define ("SITE_DESCRIPTION", "a small PHP based website to manage productkeys from your software."); // Kurzbeschreibung 
 define ("SITE_KEYWORDS", ""); // Suchwörter 
-define ("SITE_PUBLISHER", "TÜV-Nord Braunschweig"); // Veröffentlicher 
+define ("SITE_PUBLISHER", ""); // Veröffentlicher 
 
 // Datenbank 
 define ("SQL_PATH", "");	// Der verwendete Pfad zur Datenbank, z.B. localhost oder http://db.de:3306/
@@ -28,6 +28,6 @@ define ("WAIT", "3");               		// Standardwartezeit zum weiterleiten nach
 define ("VALIDATOR", "0");          	  // Anzeigen des w3c validators im Footer
 define ("SITE_LOADTIME", "1");      	  // Anzeigen der Seitenladezeit im Footer
 define ("DEBUG", "0");             		 	// Debuger 0 none, 1 only messages, 2 heavy
-define ("SALT", "asgfuUuh9842hrop8sugfkle");	// Passwdsalt
+define ("SALT", "SetYouOwn!");					// Passwdsalt
 
 ?>
